f8b5f0270783402a31965485a3ae94fa38010381
Faysal Shuvo <32492440+FaysalShuvo@users.noreply.github.com>
2021-06-01 16:35:13 +0600

    Finish Fetching Api

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import { Difficulty, fetchQuizQuestions, QuestionState } from "./API";
 
-function App() {
+const TOTAL_QUESTION = 10;
+const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState<QuestionState[]>([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+
+  console.log(fetchQuizQuestions(TOTAL_QUESTION, Difficulty.EASY));
+
+  const startTrivia = async () => {};
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+
+  const nextQuestion = () => {};
   return (
     <div className="App">
-      <h1>Learning Typescript</h1>
+      <h1>React Quiz With TypeScript</h1>
+
+      <button className="start" onClick={startTrivia}>
+        Start
+      </button>
+
+      <p className="score">Score: </p>
+      <p>Loading Question</p>
+
+      {/* <QuestionCard
+        questionNr={number + 1}
+        totalQuestions={TOTAL_QUESTION}
+        question={questions[number].question}
+        answers={questions[number].answers}
+        userAnswer={userAnswers ? userAnswers[number] : undefined}
+        callback={checkAnswer}
+      /> */}
+      <button className="next" onClick={nextQuestion}>
+        Next Question
+      </button>
     </div>
   );
-}
+};
 
 export default App;
-
-
-// https://opentdb.com/api.php?amount=10
